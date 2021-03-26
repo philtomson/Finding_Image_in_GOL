6c292517c273888cc9a15d50b640250e05d33768
Phil Tomson <philtomson@gmail.com>
2021-03-25 20:10:19 -0700

    hill climbing doing something now

--- monalisa.jl
+++ monalisa.jl
@@ -20,6 +20,9 @@ end
 md"Load up the dependencies"
 
 
+# ╔═╡ 5411b618-8dbf-11eb-1e0d-e311384e9d9d
+n_gens = 6
+
 # ╔═╡ bdd9aba4-8771-11eb-29e7-b544121ce870
 kernel = UInt8.([ 1 1 1   #kernel for GoL
                   1 0 1
@@ -36,7 +39,7 @@ function convGOL(A::AbstractMatrix, kern::AbstractMatrix)
 	outtype = eltype(A)
 	out = zeros(outtype, size(A))
 	iterize = size(A) .- size(kern)
-    @inbounds for J in CartesianIndices(out) #@avx
+    @inbounds for J in CartesianIndices(iterize) #@avx
         count = zero(outtype)
         for I ∈ CartesianIndices(kern)
             count += A[I + J] * kern[I]
@@ -47,6 +50,19 @@ function convGOL(A::AbstractMatrix, kern::AbstractMatrix)
         out[J] =  outtype(liveordie(count, A[J]))
     end
     out
+end
+
+# ╔═╡ 342aa2b0-8d1f-11eb-315d-411b639bd426
+#conv the whole "loaf"
+function convGOL_all!(A::Array{Array{UInt8,2},1}, kern::AbstractMatrix, ngens::Integer)
+   #mapslices( img -> convGOL(img, kern), A, (2,3))
+	num_imgs = size(A)[1]
+	for i in ngens
+	    for img_num in 1:num_imgs
+	        tmp = convGOL(A[img_num], kern)
+		    A[img_num] = tmp
+		end
+	end
 end
 
 # ╔═╡ f8a49e28-8830-11eb-1bc1-61485a281e82
@@ -67,6 +83,45 @@ function convGPU(AA::AbstractMatrix, k::AbstractMatrix)
     end
     out
 end
+
+# ╔═╡ 3a9216ee-8c23-11eb-02c5-1ba46ec12124
+function tileimg(img::AbstractArray, num::Integer)
+	outarray = Array{typeof(img)}(undef, num)
+	for i in 1:num
+		outarray[i] = copy(img)
+	end
+	outarray
+end
+
+# ╔═╡ 20520da4-8dbb-11eb-3712-850c67408a26
+
+
+# ╔═╡ 09742ef2-8c27-11eb-2b1d-55623b383688
+function tilerand(img, num::Integer) #will return a matrix like img
+	outarray = Array{typeof(img)}(undef, num)
+	tmp = [] #Array{Float32,2}(undef, num)
+	for i in 1:num
+		push!(tmp, eltype(img).(map( x-> round.(x), rand(Float32, size(img)))))
+		outarray[i] = eltype(img).(map( x-> round.(x), rand(Float32, size(img))))
+	end
+	#eltype(img).(map(x -> round.(x), tmp))
+	outarray
+end
+
+# ╔═╡ 186c7c1c-8c2d-11eb-374e-a70f26fbf197
+#Now we need the mutator tensor (terminology from the article)
+#Same shape as canvas_loaf, but all zeros execpt for one 1 in each "slice"
+function mutate_one(sz, kw=0)
+    mutator = zeros(UInt8, sz)
+    x,y = rand(1:sz[1]-kw), rand(1:sz[2]-kw)
+	mutator[x,y] = 0x01
+	mutator
+end
+
+# ╔═╡ 00821a34-8c2e-11eb-0e4a-09103605bb7b
+#function Base.xor(a::Array{UInt8,2}, b::Array{UInt8,2})
+#	xor.(a,b)
+#end
 
 # ╔═╡ 39fb2038-81f4-11eb-3b7f-d11eec36b65d
 begin
@@ -116,7 +171,8 @@ begin
 	batch_size = 50
 	n_generations = 6
 	width,height = size(lisa_img)
-	lisa_loaf = UInt8.(repeat(lisa_img, 1, 1, batch_size))
+	#lisa_loaf = UInt8.(repeat(lisa_img, 1, 1, batch_size))
+	lisa_loaf = tileimg(UInt8.(lisa_img), batch_size)
 	with_terminal() do
 	   @show size(lisa_loaf)
 	   @show typeof(lisa_loaf[:,:,1][1,1])
@@ -126,8 +182,11 @@ begin
 end
 
 # ╔═╡ 22d9f404-836d-11eb-3d67-0b6f64d917d4
-lisa_loaf[:,:, 1] #look at first image in "loaf"
+lisa_loaf[1] #look at first image in "loaf"
 
+
+# ╔═╡ b98ee864-8c23-11eb-3aac-bd475a3afb31
+Gray.(lisa_loaf[1])
 
 # ╔═╡ 46136f9c-836d-11eb-3d3e-75b1b2fa1e17
 #make random canvases same size/shape as lisa_loaf
@@ -135,10 +194,19 @@ begin
 	#canvas_loaf = Gray.(rand(Float32, 720,483,50))
 	#canvas_loaf[:,:,1]
 	
-	rand_loaf = Gray.(rand(Float32, 720, 483, 50))
-	canvas_loaf = UInt8.(map(x -> round(x), rand_loaf))
-	Gray.(canvas_loaf[:,:,1])
+	#rand_loaf = Gray.(rand(Float32, 720, 483, 50))
+	#canvas_loaf = UInt8.(map(x -> round(x), rand_loaf))
+	#Gray.(canvas_loaf[:,:,1])
+	
+	canvas_loaf = tilerand(lisa_loaf[1], batch_size)
+	typeof(lisa_loaf[1])
+	#ray.(canvas_loaf[1])
+	
+	
 end
+
+# ╔═╡ 33adbf88-8c2e-11eb-16fc-0baa78a3a1eb
+size(canvas_loaf)
 
 # ╔═╡ eb030774-8861-11eb-1710-c573b19b5b52
 typeof(canvas_loaf)
@@ -147,21 +215,49 @@ typeof(canvas_loaf)
 typeof(lisa_loaf)
 
 # ╔═╡ c27b583a-837d-11eb-0e68-e1d2bd95cc5e
-rmse(canvas_loaf, lisa_loaf)
+rmse_vals = rmse.(canvas_loaf, lisa_loaf)
 
-# ╔═╡ 81af2678-8773-11eb-2dbe-8f833fa742b9
-lisa_loaf[:,:,1][230,230] == 1
+# ╔═╡ a1bcff44-8dbf-11eb-19c0-b5903843c494
+min_rmse_val = minimum(rmse_vals)
 
+# ╔═╡ eec7000c-8d18-11eb-0d21-571e0939ae07
+argmin(rmse_vals)
 
+# ╔═╡ cb9803a4-8d21-11eb-21ba-f734c88bcd50
+canvas_loaf_before = copy(canvas_loaf)
 
-# ╔═╡ ebe1b162-877c-11eb-05ec-f3e4400ac8c7
-convert(UInt8, lisa_loaf[:,:,1][230,230]) == 1
+# ╔═╡ 1e3aca6c-8d23-11eb-11f2-df2a5970a8cb
+typeof(canvas_loaf_before)
 
-# ╔═╡ 5c5c6f2c-8782-11eb-34f7-3beb3a7df00a
-typeof(UInt8.(lisa_loaf[:,:,1]))
+# ╔═╡ 44ce58e2-8d23-11eb-2834-e91544367f04
+sum.(canvas_loaf_before)
+
+# ╔═╡ 02f86868-8d23-11eb-3f43-512eabce1c50
+Gray.(canvas_loaf_before[1])
+
+# ╔═╡ 0f31b2a0-8d1f-11eb-2113-510bc2e43e46
+convGOL_all!(canvas_loaf, kernel, 5)
+
+# ╔═╡ 6f5ae3b4-8d23-11eb-1e2d-891a32cfe9e8
+sum.(canvas_loaf)
+
+# ╔═╡ c821cad2-8d21-11eb-0576-618733457991
+rmse(canvas_loaf_before[1], canvas_loaf[1]) #WNY is this 0.0?!!
+
+# ╔═╡ e516ec20-8d22-11eb-3e85-d5fa4d9a062f
+Gray.(canvas_loaf[1])
+
+# ╔═╡ ba00793a-8d17-11eb-2f9d-ef2ce9565851
+rmse(canvas_loaf[46], lisa_loaf[46])
+
+# ╔═╡ 31c2c444-8d18-11eb-2820-91a3e1db3965
+Gray.(canvas_loaf[46])
+
+# ╔═╡ 3dea31aa-8d18-11eb-1d81-2f102182973f
+Gray.(lisa_loaf[46])
 
 # ╔═╡ a4ed1a44-877e-11eb-2fe9-b33ca0b1f39e
-ns1 = convGOL(lisa_loaf[:,:,1], kernel)
+ns1 = convGOL(lisa_loaf[1], kernel)
 
 # ╔═╡ 9c5aeeb6-8861-11eb-160a-b1d0222010fb
 eltype(ns1)
@@ -173,12 +269,13 @@ eltype(lisa_loaf[:,:,1])
 Gray.(ns1)
 
 # ╔═╡ 9575a962-885f-11eb-2a98-5be3d7e2d906
-for r in 1:size(canvas_loaf,3)
-	Gray.(canvas_loaf[:,:,r])
-end
+#for cnvs in canvas_loaf
+#	imshow(Gray.(cnvs))
+#end
+Gray.(canvas_loaf[1])
 
 # ╔═╡ fec3b1fc-885f-11eb-347d-99257f131975
-ns2 = convGOL(canvas_loaf[:,:,1], kernel)
+ns2 = convGOL(canvas_loaf[1], kernel)
 
 # ╔═╡ 2f717956-8860-11eb-2120-d1f872b570ec
 typeof(ns2)
@@ -192,13 +289,140 @@ ns3 = convGOL(ns2, kernel)
 # ╔═╡ 8481b3fa-8862-11eb-0faf-1be9030dba9d
 Gray.(ns3)
 
+# ╔═╡ 5de20bc6-8cf6-11eb-3e64-fd4356d32bf0
+typeof(canvas_loaf[1])
+
+
+# ╔═╡ 763f5126-8b73-11eb-0ba4-651a2b1cc8c6
+#Now we need the mutator tensor (terminology from the article)
+#Same shape as canvas_loaf, but all zeros execpt for one 1 in each "slice"
+function mutate(imgs, kw=0)
+	num_imgs = size(imgs)[1]
+	img_size = size(imgs[1])
+	outarray = Array{typeof(imgs[1])}(undef, num_imgs)
+
+	for r in 1:num_imgs
+	   mutator = zeros(UInt8, img_size)
+	   x,y = rand(1:img_size[1]-kw), rand(1:img_size[2]-kw)
+	   mutator[x,y] = 0x01
+	   outarray[r] = mutator
+    end
+	outarray
+end
+
+# ╔═╡ 69bc714e-8b74-11eb-2956-f106bef8bb3a
+mutator = mutate(canvas_loaf, size(kernel)[1])
+
+
+# ╔═╡ 2520cf00-8b7e-11eb-2e97-3d1a4d78e432
+
+size(mutator) #[699, 480] = 0x01
+
+
+# ╔═╡ b0a747bc-8cf7-11eb-3637-51d2ae76e71f
+Gray.(mutator[46])
+
+# ╔═╡ 4d276e02-8d19-11eb-1258-37b8d3877256
+#apply the mutation via xor
+xord = xor.(canvas_loaf,mutator)
+
+# ╔═╡ 03144d4c-8dc5-11eb-07ea-91afc1e4d3ad
+typeof(canvas_loaf)
+
+# ╔═╡ bedeb286-8dbd-11eb-024c-bd1c613d0792
+function hill_climb(original, canvas, iterations, kernel=kernel, num_gens=n_gens)
+	best_score  = Inf
+	best_canvas = canvas
+	s_canvas    = copy(canvas)
+	for run in 1:iterations
+		#s_canvas = xor.(s_canvas, mutate(canvas, size(kernel)[1]))
+		s_canvas = [xor.(a,b) for (a,b) in zip(s_canvas, mutate(canvas, size(kernel)[1]))]
+		convGOL_all!(s_canvas, kernel, num_gens)
+		rmse_vals = rmse.(original, s_canvas)
+		curr_min  = minimum(rmse_vals)
+		if curr_min < best_score
+			best_score = curr_min
+			best_canvas = tileimg(s_canvas[argmin(rmse_vals)], size(s_canvas)[1])
+		end
+		s_canvas = best_canvas
+	end
+	return s_canvas
+end
+
+# ╔═╡ 867e347e-8dbe-11eb-10b9-7db60f1053d8
+result = hill_climb(lisa_loaf, canvas_loaf, 5000)
+
+# ╔═╡ 68739244-8dc8-11eb-3fc0-9fd199e0ec4a
+Gray.(result[1])
+
+# ╔═╡ fedd0dac-8dc6-11eb-3594-cd1ab3de9805
+#find the best one:
+rmse_vals1 = rmse.(result, lisa_loaf)
+
+# ╔═╡ 8995305e-8dc8-11eb-362e-81fcdf7ed4cf
+minimum(rmse_vals1)
+
+# ╔═╡ bfdb81c2-8dc8-11eb-353d-71df40b46b1b
+argmin(rmse_vals1)
+
+# ╔═╡ a017b720-8dc8-11eb-3352-375f41ea9de9
+Gray.(result[argmin(rmse_vals1)])
+
+# ╔═╡ a00c0614-8dc8-11eb-1c36-1d38506baff6
+
+
+# ╔═╡ 5ba08d2c-8da4-11eb-18f0-11d1a6d22219
+modulod = (canvas_loaf[46] .+ mutator[46]) .%0x02
+
+# ╔═╡ 90354f3a-8da4-11eb-256e-6f87c0e71db9
+modulod == xord
+
+# ╔═╡ 2d338256-8d1a-11eb-2efc-6741eebf41e2
+begin
+   tensor1 = rand(UInt8, (50,720,483))
+   tensor2 = rand(UInt8, (50,720,483))
+end
+
+# ╔═╡ 48255062-8d1a-11eb-2bd3-991a511f37fd
+tensor1[1,:,:]
+
+# ╔═╡ 2249fd80-8d1e-11eb-02ea-9be207408f58
+size(tensor1[:])
+
+# ╔═╡ 91ba5812-8d1a-11eb-11d9-a31636daa7a3
+rmse(tensor1, tensor2)
+
+# ╔═╡ f0441ec2-8d1c-11eb-2fe0-05ac477f698e
+axes(tensor1)[1]
+
+# ╔═╡ 341793c0-8d1d-11eb-3bfb-4b40374f6552
+axes(tensor1,1)
+
+# ╔═╡ 370a5c86-8b7f-11eb-2769-47e295612fb1
+images = size(UInt8.(repeat(lisa_img, 1,1,50)))
+
+
+
+# ╔═╡ 10d0527c-8c0c-11eb-2580-6fc00acb6a58
+tiled_images = tileimg(UInt8.(lisa_img), 50)
+
+# ╔═╡ ec8f4f80-8c1c-11eb-21b0-2324b669c1fc
+typeof(tiled_images[1])
+
 # ╔═╡ Cell order:
 # ╠═b43bbe96-81f1-11eb-1a1f-e798274b82ac
 # ╠═a3e50b96-81f2-11eb-294d-153882ea5f67
+# ╠═5411b618-8dbf-11eb-1e0d-e311384e9d9d
 # ╠═bdd9aba4-8771-11eb-29e7-b544121ce870
 # ╠═93056986-877b-11eb-294a-0b3802b5ba62
 # ╠═150916da-8772-11eb-30e5-6b2ab6de1f24
+# ╠═342aa2b0-8d1f-11eb-315d-411b639bd426
 # ╠═f8a49e28-8830-11eb-1bc1-61485a281e82
+# ╠═3a9216ee-8c23-11eb-02c5-1ba46ec12124
+# ╠═20520da4-8dbb-11eb-3712-850c67408a26
+# ╠═09742ef2-8c27-11eb-2b1d-55623b383688
+# ╠═186c7c1c-8c2d-11eb-374e-a70f26fbf197
+# ╠═00821a34-8c2e-11eb-0e4a-09103605bb7b
 # ╠═39fb2038-81f4-11eb-3b7f-d11eec36b65d
 # ╠═0ad05da4-81f5-11eb-34fb-0fd4b2290487
 # ╠═157b5560-81f5-11eb-33b2-c5f0358999c5
@@ -210,13 +434,25 @@ Gray.(ns3)
 # ╠═c6441f9c-81f7-11eb-3481-aff6c9764877
 # ╠═df507380-81f7-11eb-2764-2504a167b1fc
 # ╠═22d9f404-836d-11eb-3d67-0b6f64d917d4
+# ╠═b98ee864-8c23-11eb-3aac-bd475a3afb31
 # ╠═46136f9c-836d-11eb-3d3e-75b1b2fa1e17
+# ╠═33adbf88-8c2e-11eb-16fc-0baa78a3a1eb
 # ╠═eb030774-8861-11eb-1710-c573b19b5b52
 # ╠═07289ae2-8862-11eb-3c83-f97cb1a1261f
 # ╠═c27b583a-837d-11eb-0e68-e1d2bd95cc5e
-# ╠═81af2678-8773-11eb-2dbe-8f833fa742b9
-# ╠═ebe1b162-877c-11eb-05ec-f3e4400ac8c7
-# ╠═5c5c6f2c-8782-11eb-34f7-3beb3a7df00a
+# ╠═a1bcff44-8dbf-11eb-19c0-b5903843c494
+# ╠═eec7000c-8d18-11eb-0d21-571e0939ae07
+# ╠═cb9803a4-8d21-11eb-21ba-f734c88bcd50
+# ╠═1e3aca6c-8d23-11eb-11f2-df2a5970a8cb
+# ╠═44ce58e2-8d23-11eb-2834-e91544367f04
+# ╠═02f86868-8d23-11eb-3f43-512eabce1c50
+# ╠═0f31b2a0-8d1f-11eb-2113-510bc2e43e46
+# ╠═6f5ae3b4-8d23-11eb-1e2d-891a32cfe9e8
+# ╠═c821cad2-8d21-11eb-0576-618733457991
+# ╠═e516ec20-8d22-11eb-3e85-d5fa4d9a062f
+# ╠═ba00793a-8d17-11eb-2f9d-ef2ce9565851
+# ╠═31c2c444-8d18-11eb-2820-91a3e1db3965
+# ╠═3dea31aa-8d18-11eb-1d81-2f102182973f
 # ╠═a4ed1a44-877e-11eb-2fe9-b33ca0b1f39e
 # ╠═9c5aeeb6-8861-11eb-160a-b1d0222010fb
 # ╠═8a34ff4e-8861-11eb-3a90-db8e79c3b476
@@ -227,3 +463,29 @@ Gray.(ns3)
 # ╠═8fce7072-8862-11eb-2704-e7de81aa35cb
 # ╠═16124688-8861-11eb-26d8-1b28de31c603
 # ╠═8481b3fa-8862-11eb-0faf-1be9030dba9d
+# ╠═5de20bc6-8cf6-11eb-3e64-fd4356d32bf0
+# ╠═763f5126-8b73-11eb-0ba4-651a2b1cc8c6
+# ╠═69bc714e-8b74-11eb-2956-f106bef8bb3a
+# ╠═2520cf00-8b7e-11eb-2e97-3d1a4d78e432
+# ╠═b0a747bc-8cf7-11eb-3637-51d2ae76e71f
+# ╠═4d276e02-8d19-11eb-1258-37b8d3877256
+# ╠═03144d4c-8dc5-11eb-07ea-91afc1e4d3ad
+# ╠═bedeb286-8dbd-11eb-024c-bd1c613d0792
+# ╠═867e347e-8dbe-11eb-10b9-7db60f1053d8
+# ╠═68739244-8dc8-11eb-3fc0-9fd199e0ec4a
+# ╠═fedd0dac-8dc6-11eb-3594-cd1ab3de9805
+# ╠═8995305e-8dc8-11eb-362e-81fcdf7ed4cf
+# ╠═bfdb81c2-8dc8-11eb-353d-71df40b46b1b
+# ╠═a017b720-8dc8-11eb-3352-375f41ea9de9
+# ╠═a00c0614-8dc8-11eb-1c36-1d38506baff6
+# ╠═5ba08d2c-8da4-11eb-18f0-11d1a6d22219
+# ╠═90354f3a-8da4-11eb-256e-6f87c0e71db9
+# ╠═2d338256-8d1a-11eb-2efc-6741eebf41e2
+# ╠═48255062-8d1a-11eb-2bd3-991a511f37fd
+# ╠═2249fd80-8d1e-11eb-02ea-9be207408f58
+# ╠═91ba5812-8d1a-11eb-11d9-a31636daa7a3
+# ╠═f0441ec2-8d1c-11eb-2fe0-05ac477f698e
+# ╠═341793c0-8d1d-11eb-3bfb-4b40374f6552
+# ╠═370a5c86-8b7f-11eb-2769-47e295612fb1
+# ╠═10d0527c-8c0c-11eb-2580-6fc00acb6a58
+# ╠═ec8f4f80-8c1c-11eb-21b0-2324b669c1fc
